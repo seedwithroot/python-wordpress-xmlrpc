c49067e3fd56d78340a00f0db3109ae26cac0408
Justin <seedwithroot@gmail.com>
2014-08-18 14:08:20 -0700

    NewUser

--- wordpress_xmlrpc/methods/users.py
+++ wordpress_xmlrpc/methods/users.py
@@ -11,7 +11,7 @@ class NewUser(AuthenticatedMethod):
 
     Returns: ID of the newly-created user id (an integer).
     """
-    method_name = '{0}.add_user'.format(NAMESPACE)
+    method_name = '{0}.callWpMethod.add_user'.format(NAMESPACE)
     method_args = ('content',)
 
 class GetUsers(AuthenticatedMethod):
